6de2442e8b35fb54ea4a32fc13f225546fbce525
Emelda Phasha <phashae25@gmail.com>
2023-03-21 12:12:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print alphabets time 10
- * then follwed by new line
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower case
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int i;
 char c;
