6a9da557acea2f429b5d136d7db0585a83316dfe
Emelda Phasha <phashae25@gmail.com>
2023-03-22 13:31:35 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: int that check
+ * Return: the absolute value if integer
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
