4a22b994cb0c8ef1ff0f9b867ef9fc3a0b1db8f7
Emelda Phasha <phashae25@gmail.com>
2023-03-30 13:39:17 -0700

    103_ add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,76 @@
+#include "main.h"
+/**
+ * rev_string - reverse array
+ * @n: integer
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+int i;
+int j;
+char temp;
+
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
+
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
+/**
+ * *infinite_add - add two numbers
+ * @n1: first number
+ * @n2: 2nd number
+ * @r: pointer to buffer
+ * @size_r: size
+ * Return: pointer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int inf = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp = 0;
+
+while (*(n1 + i) != '\0')
+	i++;
+while (*(n2 + j) != '\0')
+	j++;
+i--;
+j--;
+if (j >= size_r)
+	return (0);
+while (j >= 0 || i >= 0 || inf == 1)
+{
+if (i < 0)
+	val1 = 0;
+else
+	val1 = *(n1 + i) -'0';
+if (j < 0)
+	val2 = 0;
+else
+	val2 = *(n2 + j) -'0';
+temp = val1 +  val2 + inf;
+if (temp >= 10)
+	inf = 1;
+else
+	inf = 0;
+if (digits >= (size_r - 1))
+	return (0);
+*(r + digits) = (temp % 10) + '0';
+digits++;
+j--;
+i--;
+}
+if (digits == size_r)
+{
+	return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+}
+	return (r);
+}
