5c9f8652bed9ccad73ca9c283fa76705eccc96c4
Emelda Phasha <phashae25@gmail.com>
2023-04-03 09:02:59 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment of string
+ * @accept: bytes
+ * Return: accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+unsigned int n = 0;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+n++;
+break;
+}
+else if (accept[i + 1] == '\0')
+	return(n);
+}
+s++;
+}
+return (n);
+}
