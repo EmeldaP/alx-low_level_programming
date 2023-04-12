fb8a1f5d6873ff03c4e97be09fa17b17b96ba10c
Emelda Phasha <phashae25@gmail.com>
2023-04-12 02:39:42 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,30 +9,38 @@
 char *str_concat(char *s1, char *s2)
 {
 char *c;
-int i = 0;
-int j = 0;
+int i;
+int j;
 
 if (s1 == NULL)
-	return ("");
+	s1 = "";
 if (s2 == NULL)
-	return ("");
+	s2 ="";
+
+i = 0;
+j = 0;
 
 while (s1[i] != '\0')
-		i++;
+	i++;
 while (s2[j] != '\0')
 j++;
 c = malloc(sizeof(char) * (i + j + 1));
 
 if (c == NULL)
 return (NULL);
+i = 0;
+j = 0;
+
 while (s1[i] != '\0')
 {
 c[i] = s1[i];
 i++;
 }
+
 while (s2[j] != '\0')
 {
 c[j] = s2[j];
+i++;
 j++;
 }
 c[i] = '\0';
