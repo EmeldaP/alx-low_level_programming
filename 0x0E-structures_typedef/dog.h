30d6ec4a8d8010b70cc0ad308fafcf7037cd3ef5
Emelda Phasha <phashae25@gmail.com>
2023-04-17 13:17:11 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new dog
+ * @name: firdt member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+ * dog_d - typedef for strct dog
+ */
+typedef struct dog dog_d;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_d *new_dog(char *name, float age, char *owner);
+void free_dog(char *dest, char *src);
+int _strlen(char *s);
+#endif
